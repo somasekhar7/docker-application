f209c908e4963834972686819328b4e3b3cef053
somasekhar2356 <vdarisi@albany.edu>
2024-03-24 00:18:41 -0400

    updated server files

--- server/server.js
+++ server/server.js
@@ -1,24 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 5001;
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const path = require("path");
-// app.use(cors());
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.post("/api/addition", (req, res) => {
-//   const { num1, num2 } = req.body;
-//   const result = num1 + num2;
-//   res.json({ result });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
