573e2f3402067cd00b5ae81431db1b4f46bea47c
somasekhar7 <somasekhardarisi7@gmail.com>
2024-04-12 17:34:32 -0400

    updated the files

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,5 @@
 export const BACKEND_API_ENDPOINT =
-  process.env.BACKEND_API_ENDPOINT || "http://localhost";
-const PROD_BACKEND_API_ENDPOINT =
-  "http://ec2-18-221-207-9.us-east-2.compute.amazonaws.com:5005";
+  process.env.BACKEND_API_ENDPOINT ||
+  "http://ec2-18-117-11-175.us-east-2.compute.amazonaws.com";
+// const PROD_BACKEND_API_ENDPOINT =
+//   "http://ec2-18-221-207-9.us-east-2.compute.amazonaws.com:5005";
